ddff3bd7fc6c553b91fcd9fb8a7836ce21ee29d2
Luiz Felipe Castro <luiz@Kenetto-MBP-13.local>
2020-05-07 20:01:33 +0100

    add webpack

---
+++ build/precache-manifest.6e07db2c91bcd53c4df42c478d4b6b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d763cad105af33e7cab868bd1a888bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f59fab1aa56e825a984",
+    "url": "/static/js/2.356a5f75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.356a5f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33244ad256d5411f1fb4",
+    "url": "/static/js/main.9164cd59.chunk.js"
+  },
+  {
+    "revision": "fb1c4b129c3c9bd0ff8c",
+    "url": "/static/js/runtime-main.b096102e.js"
+  },
+  {
+    "revision": "4222733948ffb696b477136311c9ac57",
+    "url": "/static/media/bannerExtension.42227339.png"
+  },
+  {
+    "revision": "9674f1a357248dfbbee38aaabae3dbf6",
+    "url": "/static/media/icon_blue.9674f1a3.png"
+  }
+]);
